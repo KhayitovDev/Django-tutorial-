164948df9c18b7bc9fb58d3e66433f3e010d3f89
Khayitboy <khayitovkhayitboy@gmail.com>
2024-09-11 13:20:46 +0500

    imported views

--- post/views.py
+++ post/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from .models import Post 
-from .serializers import PostSerializer
+
+from rest_framework.response import Response
 
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, ListCreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+
+
+from .models import Post 
+from .serializers import PostSerializer
 
 
     
