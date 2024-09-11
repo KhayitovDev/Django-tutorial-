4f38f6ffa31b1647d546dfa69b1e9fa939eae18b
Khayitboy <khayitovkhayitboy@gmail.com>
2024-09-11 22:40:10 +0500

    added views

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,21 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+
+router.register(r"posts", views.PostViewSet)
+
+
+
 urlpatterns = [
-   
+
+    path("", include(router.urls))
+    # path("posts/", views.post_list), 
+    # path("posts/<int:pk>/", views.post_detail)
+    
+    # path("posts/", views.PostListCreateAPIView.as_view()),
+    # path("posts/<int:pk>/", views.PostRetrieveUpdateDestroyView.as_view())
+    
+    
 ]
--- post/views.py
+++ post/views.py
@@ -2,13 +2,72 @@
 from rest_framework.response import Response
 
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListAPIView, ListCreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, ListCreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
 
 from .models import Post 
 from .serializers import PostSerializer
+
+
+@api_view(['POST', 'GET'])
+def post_list(request):
+    if request.method == "GET":
+        posts = Post.objects.all()
+        serialzier = PostSerializer(posts, many=True)
+        return Response(serialzier.data)
+    elif request.method == "POST":
+        serialzier = PostSerializer(data=request.data)
+        if serialzier.is_valid():
+            serialzier.save()
+            return Response(serialzier.data, status=201)
+        return Response(serialzier.errors, status=400)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def post_detail(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return Response(status=404)
+    
+    if request.method == "GET":
+        serialzier = PostSerializer(post)
+        return Response(serialzier.data)
+    elif request.method == "PUT":
+        serialzier = PostSerializer(post, data=request.data, partial = True)
+        if serialzier.is_valid():
+            serialzier.save()
+            return Response(serialzier.data)
+        return Response(serialzier.errors, status=400)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response(status=204)
+    
+class PostListCreateAPIView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+
+        
+        
+
+        
+
+    
+
+
+
+
+   
 
 
 
